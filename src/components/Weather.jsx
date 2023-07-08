f4de203c0b771c0007874305b840324d43ef11d0
Jose Gentil Barreto <compucalima@hotmail.com>
2023-07-08 16:22:44 -0500

    Solución algunos telefonos no muestran temperatura

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,74 +2,100 @@ import axios from "axios"
 import { useState, useEffect } from "react"
 import Wait from "./Wait"
 const Weather = () => {
-    const [ data, setData ] = useState({})
-    const [countrys, setCountrys] = useState('')
-    const [onLoad, setOnLoad] = useState(true)
-    const [temperature, setTemperature] = useState(true)
-    const changeTemp = () => setTemperature(!temperature)
-    useEffect(()=> {
-        navigator.geolocation.getCurrentPosition(function(Position){
-            const Latitude = Position.coords?.latitude
-            const Longitude = Position.coords?.longitude
-            const weaterUrl1= `https://api.openweathermap.org/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&appid=55499aeb43c1f14e789bc948de62d7f3&lang=es&units=metric`
-           
-            console.log(Latitude, Longitude);
-            axios.get(weaterUrl1).then((resp) =>{
-                setData(resp.data) 
-                setOnLoad(false)
-            })
-        },function(error){
-            console.error(error);
+  const [ data, setData ] = useState({})
+  const [countrys, setCountrys] = useState('')
+  const [onLoad, setOnLoad] = useState(true)
+  const [temperature, setTemperature] = useState(true)
+  const changeTemp = () => setTemperature(!temperature)
+    
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition(function(Position){
+      const Latitude = Position.coords?.latitude
+      const Longitude = Position.coords?.longitude
+      const weaterUrl1= `https://api.openweathermap.org/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&appid=55499aeb43c1f14e789bc948de62d7f3&lang=es&units=metric`
+       
+      console.log(Latitude, Longitude);
+      axios.get(weaterUrl1).then((resp) =>{
+        setData(resp.data) 
+        setOnLoad(false)
+        })
+      },function(error){
+        console.error(error);
         },{enableHighAccuracy: true})
 
     },[])  
     const weaterUrl = `https://api.openweathermap.org/data/2.5/weather?q=${countrys}&appid=55499aeb43c1f14e789bc948de62d7f3&lang=es&units=metric`
     const searchCountry = (event) => {
-        if(event.key === 'Enter'){
-            axios.get(weaterUrl).then((resp) =>{
-                setData(resp.data)
-                console.error(resp.data.weather[0].icon);
-            })
-        }
+      if(event.key === 'Enter'){
+        axios.get(weaterUrl).then((resp) =>{
+          setData(resp.data)
+          console.error(resp.data.weather[0].icon);
+       })
+      }
     }
+
+
+    let temperatureNoNaN;
+    if(!isNaN(temperature)) {
+      if(temperature){
+        temperatureNoNaN= Math.round(data.main?.temp) + "°C";
+      } else {
+        temperatureNoNaN= Math.round((data.main?.temp * 9/5) + 32)+ "°F";
+      }
+    } else {
+      temperatureNoNaN ="-"
+    }
+
     return (
-        <>
-            <section className='todo'>{ onLoad && <Wait /> }
-                <div className="top__container">
-                    <h1 className="title__app">Weather app</h1>
-                    <input value={countrys}  onChange={event => setCountrys(event.target.value)} placeholder="País y presione enter" onKeyPress={searchCountry} type="text" name="" id="" className="search__country" />
+      <>
+        <section className='todo'>{ onLoad && <Wait /> }
+          <div className="top__container">
+            <h1 className="title__app">Weather app</h1>
+            <input value={countrys}  onChange={event => setCountrys(event.target.value)} placeholder="País y presione enter" onKeyPress={searchCountry} type="text" name="" id="" className="search__country" />
+          </div>
+          <div className="container">
+            <div className="center__container">
+              <div className="clouds">
+                {data.weather && data.weather.length > 0 && (<img className="image" src={`./img/${data.weather[0]?.icon}.svg`} alt="" />)}                        
+                
+                <p className="p__center">{temperatureNoNaN}</p>
+                {/* <p className="p__center">
+                  {temperature ? 
+                    Math.round(data.main?.temp) + "°C" 
+                  : 
+                    Math.round((data.main?.temp * 9/5) + 32) + "°F" } 
+                </p> */}
+                
+
+
+
+
+              </div>
+              <div className="wtp__container">
+                <div className="wind">
+                  <p className="labels">Vientos:</p>
+                  <p>{data.wind?.speed} k/h</p>
                 </div>
-                <div className="container">
-                    <div className="center__container">
-                        <div className="clouds">
-                            {data.weather && data.weather.length > 0 && (<img className="image" src={`./img/${data.weather[0]?.icon}.svg`} alt="" />)}                        
-                            <p className="p__center">{temperature ? Math.round(data.main?.temp) + "°C" : Math.round((data.main?.temp * 9/5) + 32) + "°F" } </p>
-                        </div>
-                        <div className="wtp__container">
-                            <div className="wind">
-                                <p className="labels">Vientos:</p>
-                                <p>{data.wind?.speed} k/h</p>
-                            </div>
-                            <div className="clouds__now">
-                                <p className="labels">Nubes:</p>
-                                {data.weather && data.weather.length > 0 && (<p>{data.weather[0]?.description}</p>)}
-                            </div>
-                            <div className="pressure">
-                                <p className="labels">Presión:</p>
-                                <p>{data.main?.pressure}</p>
-                            </div>
-                        </div>
-                        <div className="country">
-                            <p>Country:</p>
-                            <p>{data.name}</p>
-                        </div>
-                    </div>           
+                <div className="clouds__now">
+                  <p className="labels">Nubes:</p>
+                  {data.weather && data.weather.length > 0 && (<p>{data.weather[0]?.description}</p>)}
                 </div>
-                <div className="botton__container">
-                    <button className="button" onClick={changeTemp}>Celsius / Farenheit</button>
+                <div className="pressure">
+                  <p className="labels">Presión:</p>
+                  <p>{data.main?.pressure}</p>
                 </div>
-            </section>
-        </>
+              </div>
+              <div className="country">
+                <p>Country:</p>
+                <p>{data.name}</p>
+              </div>
+            </div>           
+          </div>
+          <div className="botton__container">
+            <button className="button" onClick={changeTemp}>Celsius / Farenheit</button>
+          </div>
+        </section>
+      </>
     )
 }
 export default Weather
